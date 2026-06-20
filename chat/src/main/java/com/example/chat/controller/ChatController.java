d31842ae16a4de58b5ad6bb98239b8097b524937
HSH-11 <lyk1517@naver.com>
2026-06-20 16:42:02 +0900

    OpenAI API를 활용한 채팅 API 환경 구축

---
+++ chat/src/main/java/com/example/chat/controller/ChatController.java
@@ -0,0 +1,24 @@
+package com.example.chat.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    // chatClient 빌더 객체 생성
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/ai")
+    public String generation(String userPrompt) {
+        return this.chatClient.prompt()
+                .user(userPrompt)
+                .call()
+                .content();
+    }
+}
